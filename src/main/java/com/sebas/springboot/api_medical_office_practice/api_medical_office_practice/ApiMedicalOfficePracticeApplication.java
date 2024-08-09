7d8d92fe7cb594a6396babf5db0957a9013e65d6
sebastian-VB <sebas99peru@gmail.com>
2024-08-09 12:17:25 -0500

    first commit

---
+++ src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/ApiMedicalOfficePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sebas.springboot.api_medical_office_practice.api_medical_office_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMedicalOfficePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMedicalOfficePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/ApiMedicalOfficePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebas.springboot.api_medical_office_practice.api_medical_office_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMedicalOfficePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
