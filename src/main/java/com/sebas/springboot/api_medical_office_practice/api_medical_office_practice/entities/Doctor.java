5d4c25888ae770557681a9849fd2cb38d4d51341
sebastian-VB <sebas99peru@gmail.com>
2024-08-13 09:39:28 -0500

    Add table tag with name in entities

--- src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/entities/Doctor.java
+++ src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/entities/Doctor.java
@@ -6,8 +6,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "doctor")
 public class Doctor {
 
     @Id
