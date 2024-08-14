5d58354128f3eea9612a313f6527ce4a43f6fe37
sebastian-VB <sebas99peru@gmail.com>
2024-08-14 10:44:43 -0500

    Change joincolumn tag for column tag

--- src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/entities/Recipe.java
+++ src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/entities/Recipe.java
@@ -3,6 +3,7 @@ package com.sebas.springboot.api_medical_office_practice.api_medical_office_prac
 import java.sql.Date;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,11 +28,11 @@ public class Recipe {
     private String diagnosis;
 
     @ManyToOne
-    @JoinColumn(name = "doctor_id")
+    @Column(name = "doctor_id")
     private Doctor doctor;
 
     @ManyToOne
-    @JoinColumn(name = "patient_id")
+    @Column(name = "patient_id")
     private Patient patient;
 
     @ManyToMany
