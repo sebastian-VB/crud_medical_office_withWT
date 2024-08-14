76a53928862b1f60e3aa379ff8cbc77cddf12a4f
sebastian-VB <sebas99peru@gmail.com>
2024-08-14 12:26:56 -0500

    Update save method

--- src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/service/DoctorServiceImpl.java
+++ src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/service/DoctorServiceImpl.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.entities.Doctor;
+import com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.entities.Person;
 import com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.repositories.DoctorRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class DoctorServiceImpl implements DoctorService{
@@ -26,8 +29,20 @@ public class DoctorServiceImpl implements DoctorService{
         return null;
     }
 
+    @Transactional
     @Override
     public Doctor save(Doctor doctor) {
+
+        Person newPerson = new Person(
+            doctor.getPerson().getName(), 
+            doctor.getPerson().getLastname(),
+            doctor.getPerson().getDni(), 
+            doctor.getPerson().getPhone()
+        );
+        
+        Doctor newDoctor = new Doctor(doctor.getSpeciality());
+        newDoctor.setPerson(newPerson);
+
         return doctorRepository.save(doctor);
     }
 
