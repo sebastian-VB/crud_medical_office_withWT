03b48daa68333cbe2fd7bba8b86700cc7950e615
sebastian-VB <sebas99peru@gmail.com>
2024-08-19 13:42:55 -0500

    Create endpoints for medicine

---
+++ src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/service/MedicineServiceImpl.java
@@ -0,0 +1,52 @@
+package com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.entities.Medicine;
+import com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.repositories.MedicineRepository;
+
+@Service
+public class MedicineServiceImpl implements MedicineService{
+
+    @Autowired
+    private MedicineRepository medicineRepository;
+
+    @Override
+    public List<Medicine> findAll() {
+        return medicineRepository.findAll();
+    }
+
+    @Override
+    public Optional<Medicine> findById(Long id) {
+        return medicineRepository.findById(id);
+    }
+
+    @Transactional
+    @Override
+    public Medicine save(Medicine medicine) {
+        return medicineRepository.save(medicine);
+    }
+
+    @Transactional
+    @Override
+    public Optional<Medicine> update(Medicine medicine, Long id) {
+        
+        Optional<Medicine> optionalMedicine = medicineRepository.findById(id);
+
+        if(optionalMedicine.isPresent()){
+            Medicine medicineDb = optionalMedicine.orElseThrow();
+            medicineDb.setCode(medicine.getCode());
+            medicineDb.setName(medicine.getName());
+
+            medicineRepository.save(medicineDb);
+        }
+
+        return optionalMedicine;
+    }
+
+}
