c6024016cd6a3ecb1811915adecbf5c92eb9a3b5
sebastian-VB <sebas99peru@gmail.com>
2024-08-28 10:14:24 -0500

    Add new method find by dni

--- src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/repositories/PersonRepository.java
+++ src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/repositories/PersonRepository.java
@@ -1,5 +1,7 @@
 package com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.entities.Person;
@@ -7,4 +9,5 @@ import com.sebas.springboot.api_medical_office_practice.api_medical_office_pract
 public interface PersonRepository extends JpaRepository<Person, Long>{
 
     boolean existsByDni(String dni);
+    Optional<Person> findByDni(String dni);
 }
--- src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/service/PersonServiceImpl.java
+++ src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/service/PersonServiceImpl.java
@@ -1,8 +1,11 @@
 package com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.entities.Person;
 import com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.repositories.PersonRepository;
 
 @Service
@@ -12,8 +15,19 @@ public class PersonServiceImpl implements PersonService{
     private PersonRepository personRepository;
 
     @Override
+    public Optional<Person> findByDni(String dni) {
+        
+        Optional<Person> optionalPerson = personRepository.findByDni(dni);
+
+        if(optionalPerson.isPresent()){
+            return Optional.of(optionalPerson.orElseThrow());
+        }
+
+        return optionalPerson;
+    }
+
+    @Override
     public boolean existsByDni(String dni) {
         return personRepository.existsByDni(dni);
     }
-
 }
