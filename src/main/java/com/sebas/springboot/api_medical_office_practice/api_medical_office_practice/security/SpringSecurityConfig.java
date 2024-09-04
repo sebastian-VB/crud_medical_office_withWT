7b588b4e52ceee74cccdd2b54de06f270b48b7c0
sebastian-VB <sebas99peru@gmail.com>
2024-09-04 13:01:42 -0500

    Add filterchain method

--- src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/security/SpringSecurityConfig.java
+++ src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/security/SpringSecurityConfig.java
@@ -2,8 +2,11 @@ package com.sebas.springboot.api_medical_office_practice.api_medical_office_prac
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -11,6 +14,17 @@ public class SpringSecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http.authorizeHttpRequests((authz) -> authz
+            .requestMatchers("/api/users").permitAll()
+            .anyRequest().authenticated()
+        )
+            .csrf(config -> config.disable())
+            .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
     }
 
 }
