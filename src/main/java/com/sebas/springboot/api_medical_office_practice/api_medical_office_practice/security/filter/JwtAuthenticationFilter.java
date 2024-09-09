abdbc712ef673b9a01b64d651c0695b15b1de7c6
sebastian-VB <sebas99peru@gmail.com>
2024-09-09 11:56:53 -0500

    Add successfulAuthentication method for create token

--- src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/security/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,10 @@
 package com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.security.filter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,12 +17,17 @@ import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.entities.User;
 
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
     private AuthenticationManager authenticationManager;
+
+    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
@@ -47,6 +56,30 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
 
         return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+        Authentication authResult) throws IOException, ServletException {
+        
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User)authResult.getPrincipal();
+        String username = user.getUsername();
+
+        String token = Jwts.builder()
+                        .subject(username)
+                        .signWith(SECRET_KEY)
+                        .compact();
+        
+        response.addHeader("Authorization", "Bearer " + token);
+
+        Map<String, String> body = new HashMap<>();
+        body.put("token", token);
+        body.put("username", username);
+        body.put("message", String.format("Hola %s hsa iniciado sesión con éxito!", username));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType("application/json");
+        response.setStatus(200);
     }
 
     
