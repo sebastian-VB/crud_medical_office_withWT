f478b6779cac05b7b1121c6cbcba90e7f9a31d66
sebastian-VB <sebas99peru@gmail.com>
2024-09-10 13:36:19 -0500

    Create spring security config

--- src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/security/SpringSecurityConfig.java
+++ src/main/java/com/sebas/springboot/api_medical_office_practice/api_medical_office_practice/security/SpringSecurityConfig.java
@@ -1,30 +1,45 @@
 package com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.security;
 
-// import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.config.http.SessionCreationPolicy;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.sebas.springboot.api_medical_office_practice.api_medical_office_practice.security.filter.JwtAuthenticationFilter;
 
 @Configuration
 public class SpringSecurityConfig {
 
-    // @Bean
-    // PasswordEncoder passwordEncoder(){
-    //     return new BCryptPasswordEncoder();
-    // }
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
-    // @Bean
-    // SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-    //     return http.authorizeHttpRequests((authz) -> authz
-    //         .requestMatchers("/api/users").permitAll()
-    //         .anyRequest().authenticated()
-    //     )
-    //         .csrf(config -> config.disable())
-    //         .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-    //         .build();
-    // }
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http.authorizeHttpRequests((authz) -> authz
+            .requestMatchers("/api/doctor").permitAll()
+            .requestMatchers("/api/doctor/list").permitAll()
+            .anyRequest().authenticated()
+        )
+            .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+            .csrf(config -> config.disable())
+            .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
+    }
 
 }
